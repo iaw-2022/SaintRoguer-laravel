05cfc08706437d512af5dbd00dea508e1dd671c3
SaintRoguer <anonymousvsworld@gmail.com>
2022-03-22 19:10:45 -0300

    Added login and seeder.
    2 normal users and 10 random ones.

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Trailerama'),
 
     /*
     |--------------------------------------------------------------------------
@@ -176,6 +176,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\FortifyServiceProvider::class,
+        App\Providers\JetstreamServiceProvider::class,
 
     ],
 
